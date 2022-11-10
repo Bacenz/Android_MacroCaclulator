5e13b8b0d2d64079060a810f7dc72012eb845aff
Bacenz <bentheuchiha@gmail.com>
2022-11-11 00:14:01 +0700

    some information layout

--- app/src/main/java/com/example/assignment1/InformationActivity.java
+++ app/src/main/java/com/example/assignment1/InformationActivity.java
@@ -3,14 +3,12 @@ package com.example.assignment1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class InformationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_information);
     }
 }

d994d930ddda23221d82291f9b54a6a9d732ba78
Bacenz <bentheuchiha@gmail.com>
2022-11-11 11:03:50 +0700

    add activity information

--- app/src/main/java/com/example/assignment1/InformationActivity.java
+++ app/src/main/java/com/example/assignment1/InformationActivity.java
@@ -3,6 +3,9 @@ package com.example.assignment1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 public class InformationActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class InformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+        SeekBar seekBarAge = (SeekBar) findViewById(R.id.seekBarAge);
+        TextView textSeekBarAge = (TextView) findViewById(R.id.textSeekBarAge);
+
+        seekBarAge.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int temp = i + 18;
+                textSeekBarAge.setText(Integer.toString(temp));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
